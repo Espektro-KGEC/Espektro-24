6afc85a97adf3a670a9ec066fe7b3dad2b030aaa
KrishnenduDakshi2002 <kdakshi2020@gmail.com>
2024-02-24 23:14:36 +0530

    navbar added with changed styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
-function App() {
-  return <div>App</div>;
-}
+import { FunctionComponent } from 'react';
+
+import {
+  Route,
+  Routes,
+} from 'react-router';
+
+import Layout from '@components/PageLayout/Layout';
+
+const App: FunctionComponent = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<div>Home</div>} />
+        <Route path="about" element={<div>About</div>} />
+      </Route>
+    </Routes>
+  );
+};
 
 export default App;
